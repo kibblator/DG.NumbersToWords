5ed1460061741e9d927be3a2021875126230e037
Daniel Gregson <kornfreakuk@hotmail.com>
2019-12-05 21:16:48 +0000

    Added other simple tests for obvious numbers

--- tests/DG.NumbersToWordsTests/NumbersToWordsTests.cs
+++ tests/DG.NumbersToWordsTests/NumbersToWordsTests.cs
@@ -26,5 +26,49 @@ namespace DG.NumbersToWordsTests
             //Assert
             Assert.That(wordString, Is.EqualTo(expectedWordString));
         }
+
+        [Test]
+        [TestCase(11, "Eleven")]
+        [TestCase(12, "Twelve")]
+        [TestCase(13, "Thirteen")]
+        [TestCase(14, "Fourteen")]
+        [TestCase(15, "Fifteen")]
+        [TestCase(16, "Sixteen")]
+        [TestCase(17, "Seventeen")]
+        [TestCase(18, "Eighteen")]
+        [TestCase(19, "Nineteen")]
+        public void SpecialNumbersAboveTen_DisplaysInWords(int number, string expectedWordString)
+        {
+            //Arrange
+            var converterService = new NumberConverterService();
+
+            //Act
+            var wordString = converterService.ConvertNumberToWord(number);
+
+            //Assert
+            Assert.That(wordString, Is.EqualTo(expectedWordString));
+        }
+
+        [Test]
+        [TestCase(10, "Ten")]
+        [TestCase(20, "Twenty")]
+        [TestCase(30, "Thirty")]
+        [TestCase(40, "Forty")]
+        [TestCase(50, "Fifty")]
+        [TestCase(60, "Sixty")]
+        [TestCase(70, "Seventy")]
+        [TestCase(80, "Eighty")]
+        [TestCase(90, "Ninety")]
+        public void MultiplesOfTen_DisplaysInWords(int number, string expectedWordString)
+        {
+            //Arrange
+            var converterService = new NumberConverterService();
+
+            //Act
+            var wordString = converterService.ConvertNumberToWord(number);
+
+            //Assert
+            Assert.That(wordString, Is.EqualTo(expectedWordString));
+        }
     }
 }
